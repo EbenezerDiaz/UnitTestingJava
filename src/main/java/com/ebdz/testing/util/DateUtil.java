ed785eaca439cd6719bc0c8ed91dea060a7da799
Ebenezer <diaz.ebenezer@gmail.com>
2020-12-27 09:53:00 -0600

    add class and test for DateUtilLeapYear

--- src/main/java/com/ebdz/testing/util/DateUtil.java
+++ src/main/java/com/ebdz/testing/util/DateUtil.java
@@ -1,4 +1,10 @@
 package com.ebdz.testing.util;
 
 public class DateUtil {
+
+    public static boolean isLeapYear(int year){
+
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
 }
