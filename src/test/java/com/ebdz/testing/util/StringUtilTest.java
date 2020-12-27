c38c6e6a414f92caae216e807cdbc9a083511749
Ebenezer <diaz.ebenezer@gmail.com>
2020-12-27 07:37:33 -0600

    add negative validation

--- src/test/java/com/ebdz/testing/util/StringUtilTest.java
+++ src/test/java/com/ebdz/testing/util/StringUtilTest.java
@@ -9,32 +9,26 @@ public class StringUtilTest {
 
     @Test
     public void testRepeatThreeTimes() {
-
         assertEquals( "holaholahola", StringUtil.repeat("hola", 3));
-
     }
 
     @Test
     public void testRepeatTwoTimes() {
-
         assertEquals( "holahola", StringUtil.repeat("hola", 2));
     }
 
     @Test
     public void testRepeatOneTime() {
-
         assertEquals( "hola", StringUtil.repeat("hola", 1));
     }
 
     @Test
     public void testRepeatZeroTimes() {
-
         assertEquals( "", StringUtil.repeat("hola", 0));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testRepeatNegativeTimes() {
-
          StringUtil.repeat("hola", -1);
     }
 }
