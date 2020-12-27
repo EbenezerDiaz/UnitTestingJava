cf2082afb6e78d734f12875b172a960d3ad5a569
Ebenezer <diaz.ebenezer@gmail.com>
2020-12-26 20:44:06 -0600

    Se crea el proyecto

---
+++ src/main/java/com/ebdz/testing/UnitTestingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ebdz.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebdz/testing/UnitTestingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebdz.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
