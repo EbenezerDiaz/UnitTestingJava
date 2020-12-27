c3e2d754d96caafa99b7cfd3666de482cd9090be
Ebenezer <diaz.ebenezer@gmail.com>
2020-12-27 07:55:51 -0600

    add method play_win_when_dice_number_is_big

--- src/test/java/com/ebdz/testing/player/PlayerTest.java
+++ src/test/java/com/ebdz/testing/player/PlayerTest.java
@@ -19,5 +19,17 @@ public class PlayerTest {
         assertFalse(player.play());
     }
 
-    
+    @Test
+    public void play_win_when_dice_number_is_big() {
+
+        Dice dice = Mockito.mock(Dice.class);
+
+        Mockito.when(dice.roll()).thenReturn(4);
+
+        Player player = new Player(dice, 3);
+
+        assertFalse(player.play());
+    }
+
+
 }
