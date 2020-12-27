fdb12f1fc8344419f45c7ad2f6d6b2e6ef855181
Ebenezer <diaz.ebenezer@gmail.com>
2020-12-27 07:53:32 -0600

    add method play_looses_when_dice_number_is_too_low

--- src/test/java/com/ebdz/testing/player/PlayerTest.java
+++ src/test/java/com/ebdz/testing/player/PlayerTest.java
@@ -1,12 +1,21 @@
 package com.ebdz.testing.player;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
     @Test
-    public void play() {
+    public void play_looses_when_dice_number_is_too_low() {
+
+        Dice dice = Mockito.mock(Dice.class);
+
+        Mockito.when(dice.roll()).thenReturn(2);
+
+        Player player = new Player(dice, 3);
+
+        assertEquals(false, player.play());
     }
 }
